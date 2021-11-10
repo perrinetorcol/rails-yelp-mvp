6738f70de0adb791f2efd800070e58c37c0ef6ed
Perrine Torcol <perrine.trcl@gmail.com>
2021-11-10 12:40:51 +0100

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+epicure = Restaurant.new(name: "Epicure", category: "french", address: "bordeaux")
+epicure.save
+tripletta = Restaurant.new(name: "Tripletta", category: "italian", address: "saint michel")
+tripletta.save
+pavlov = Restaurant.new(name: "Le Chien de Pavlov", category: "french", address: "rue de la devise")
+pavlov.save
+kokomo = Restaurant.new(name: "Kokomo Café", category: "french", address: "rue ravez")
+kokomo.save
+bibimbap = Restaurant.new(name: "Bibimbap", category: "japanese", address: "place camille julian")
+bibimbap.save
+ploucs = Restaurant.new(name: "Chez les Ploucs", category: "french", address: "rue du parlement saint pierre")
+ploucs.save
